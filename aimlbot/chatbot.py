d1d4348bcfc7d1ac33ec5e0c2402b79ad30a7515
isro01 <ishir@iitk.ac.in>
2020-05-13 16:41:45 +0530

    coursera assignments

---
+++ aimlbot/chatbot.py
@@ -0,0 +1,18 @@
+import aiml
+import os
+
+kernel = aiml.Kernel()
+
+# kernel.learn("std-startup.xml")
+# kernel.respond("load aiml b")
+
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile= "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
+    kernel.saveBrain("bot_brain.brn")
+
+while True:
+    print (kernel.respond(raw_input("enter your message ==> ")))
+
+
